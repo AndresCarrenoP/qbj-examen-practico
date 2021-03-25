13bcf2625a5817020fd4c65a8f0a30b4496c1f86
AndresCarrenoP <andres.carreno@live.com>
2021-03-24 19:15:19 -0600

    Update Fecha class

--- src/Fecha/Fecha.java
+++ src/Fecha/Fecha.java
@@ -1,4 +1,12 @@
 package Fecha;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Fecha {
+
+    public static String getFechaHoy(){
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
+    }
+
 }
