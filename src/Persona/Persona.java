88c57c58437f5b125bbd7f221084f7fd9c38e88e
AndresCarrenoP <andres.carreno@live.com>
2021-03-25 21:37:22 -0600

    Update Persona class

--- src/Persona/Persona.java
+++ src/Persona/Persona.java
@@ -1,4 +1,40 @@
 package Persona;
 
-public class Persona {
+public abstract class Persona {
+    private static String nombre;
+    private static String apellido;
+
+
+    //constructor default
+    public Persona (){}
+
+    //"un constructor que acepte el nombre de la persona como parámetro de entrada"
+    //YAGNI :D
+    public Persona (String nombre){
+        this.nombre =  nombre;
+    }
+
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+
+    /* "finalmente agrega un Comportamiento llamado imprimirNombre y que imprima el nombre y el apellido."
+    En lugar de imprimirNombre se llamará imprimirDetalles */
+    public abstract void imprimirDetalles();
+
 }
