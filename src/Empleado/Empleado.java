bae3cdc074f5e80922d1d35459b60607bc36851c
AndresCarrenoP <andres.carreno@live.com>
2021-03-25 21:38:17 -0600

    Update Empleado class

--- src/Empleado/Empleado.java
+++ src/Empleado/Empleado.java
@@ -1,4 +1,52 @@
 package Empleado;
 
-public class Empleado {
+import Persona.Persona;
+import Fecha.Fecha;
+
+public class Empleado extends Persona{
+    private int id;
+    private long nss;
+    private String fechaingreso;
+
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+
+    public void setNss(long nss){
+        this.nss = nss;
+    }
+
+
+    public long getNss(){
+        return nss;
+    }
+
+
+    public void setFechaIngreso(String fechaingreso){
+        this.fechaingreso = fechaingreso;
+    }
+
+
+    public String getFechaIngreso(){
+        return fechaingreso;
+    }
+
+
+    @Override
+    public void imprimirDetalles(){
+        System.out.println("ID de Empleado: " + id);
+        System.out.println("Nombre: " + super.getNombre());
+        System.out.println("Apellido: " + super.getApellido());
+        System.out.println("NSS: " + nss);
+        this.setFechaIngreso(Fecha.getFechaHoy());
+        System.out.println("Fecha de Ingreso: " + fechaingreso);
+    }
+
+
 }
