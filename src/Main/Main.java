8d86d80ad0a81051b1c93e18c9d3c87ca08bcbff
AndresCarrenoP <andres.carreno@live.com>
2021-03-25 21:39:08 -0600

    Update Main class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,18 @@
 package Main;
 
+import Empleado.Empleado;
+
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\nPre-registro de nuevos empleados\nIntroduzca el nombre del empleado y presione la tecla Enter");
+
+        Empleado nvoemp = new Empleado();
+        nvoemp.setNombre("Juan");
+        nvoemp.setApellido("Cuerdas");
+        nvoemp.setId(12345);
+        nvoemp.setNss(12345678910L);
+        nvoemp.imprimirDetalles();
+
+    }
 }
